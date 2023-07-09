494211b88e073d9a5d5737b9bff7352ee34a4d36
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-07-09 22:09:32 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,6 @@ export const App = () => {
                   <h2>Contacts</h2>
                   <Filter/>
                   <ContactList />
-              </div> 
+              </div>
             </section>
 };
